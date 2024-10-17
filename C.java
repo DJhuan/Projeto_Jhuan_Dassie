b10e09e68ec1b7bdd781a38c88d4cb44b623eb92
DJhuan <jhuancarlossd@proton.me>
2024-10-17 17:44:58 -0300

    Adicionado construtor para classe C

--- C.java
+++ C.java
@@ -2,6 +2,11 @@ public class C {
     private String C1;
     private int C2;
 
+    public C(String C1, int C2){
+        this.C2 = C2;
+        this.C1 = C1;
+    }
+
     public void MC1(){
         System.out.println("Nome do método: 'MC1'");
     }
