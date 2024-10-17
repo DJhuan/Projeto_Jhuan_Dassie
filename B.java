b6e817291892e12a5243a7820ceb37e5ec97a9f8
DJhuan <jhuancarlossd@proton.me>
2024-10-17 17:41:37 -0300

    Adicionado construtor para classe B

--- B.java
+++ B.java
@@ -2,6 +2,11 @@ public class B {
     private int B1;
     private float B2;
 
+    public B(int B1, float B2){
+        this.B1 = B1;
+        this.B2 = B2;
+    }
+
     public int getB1() {
         return B1;
     }
