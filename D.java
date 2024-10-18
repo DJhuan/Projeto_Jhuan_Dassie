b6560fb325382615f4358add9ae8420f0971959c
DJhuan <jhuancarlossd@proton.me>
2024-10-18 08:54:31 -0300

    Implementada a classe D

---
+++ D.java
@@ -0,0 +1,18 @@
+public class D {
+    private char D1;
+    private double D2;
+
+    public D(char D1, double D2){
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    private void MD1(String mensagem){
+        System.out.println("Mensagem importante: " + mensagem);
+    }
+
+    public void MD2(){
+        MD1("Estes são os dados deste objeto: ");
+        System.out.println("D1: " + D1 + " D2: " + D2);
+    }
+}
